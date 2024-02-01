5dad2011faa3f87c195687a63572a9a2d50d074d
guilherme <guilhermer_mesquita@hotmail.com>
2024-02-01 17:07:34 -0400

    feat: create structure for login and add font IBM

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,5 @@
 import { Input, RoudedButton, RoudedButtonColorized } from "../../components"
+import Logo from '../../assets/logo/logo.png'
 
 export const Login = () => {
     const estiloLinha = {
@@ -6,19 +7,24 @@ export const Login = () => {
         color: 'blue',       // Cor da linha
         backgroundColor: 'blue',  // Cor de fundo da linha (pode afetar a aparência em alguns navegadores)
         height: '2px',       // Espessura da linha
-      };
+    };
     return (
-        <div>
-            <h1>teste</h1>
+        <main>
+            <section>
+                <img src={Logo}  alt="Descrição da imagem" height='10%'/>
+                <h1>LabEddit</h1>
+                <p>O projeto de rede social da Labenu</p>
+            </section>
+
             <form action="/" method="post">
                 <Input content="email" type="email" />
                 <Input content="senha" type="password" />
-                <RoudedButtonColorized content="continuar"/>
+                <RoudedButtonColorized content="continuar" />
             </form>
-            <hr style={estiloLinha}/>
+            <hr style={estiloLinha} />
             <form action="/" method="post">
                 <RoudedButton content="Crie uma conta!" />
             </form>
-        </div>
+        </main>
     )
 }
